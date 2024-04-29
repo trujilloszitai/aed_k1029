a4d726654c9527a89e590b35df973e95d2161b16
trujilloszitai <trujilloszitai@gmail.com>
2024-04-29 13:34:45 -0300

    summary details

--- practica1/ejer05-1.1/main.cpp
+++ practica1/ejer05-1.1/main.cpp
@@ -1,6 +1,47 @@
 #include <iostream>
 using namespace std;
 
+bool is_triangle(int, int, int);
+int triangle_type(int, int, int);
+
+int main()
+{
+    // La funcion principal recibe 3 valores que representan los lados de una figura: determinar, en caso de ser un triangulo, de qué tipo se trata
+
+    int s1, s2, s3, res;
+    s1 = s2 = s3 = 0;
+    cout << "HOLA USUARIO. A continuación deberá ingresar por teclado los tres valores que representan a los lados de una figura. Si se llega a determinar que es un triángulo, veremos si es equilátero, isósceles o escaleno." << endl;
+    cout << "Ingrese el primer valor: ";
+    cin >> s1;
+    cout << "Ingrese el segundo valor: ";
+    cin >> s2;
+    cout << "Ingrese el terecer valor: ";
+    cin >> s3;
+
+    if (is_triangle(s1, s2, s3))
+    {
+        res = triangle_type(s1, s2, s3);
+        switch (res)
+        {
+        case -1:
+            cout << "Los valores ingresados forman un triángulo isósceles." << endl;
+            break;
+        case 0:
+            cout << "Los valores ingresados forman un triángulo equilátero." << endl;
+            break;
+        case 1:
+            cout << "Los valores ingresados forman un triángulo escaleno." << endl;
+            break;
+        default:
+            break;
+        }
+    }
+    else
+    {
+        cout << "Los valores ingresados no forman un triángulo." << endl;
+    }
+}
+
 /**
  * @brief  Recibe 3 valores enteros y determina si se trata de un triángulo.
  * 
@@ -39,43 +80,5 @@ int triangle_type(int s1, int s2, int s3)
     else
     {
         return 1;
-    }
-}
-
-int main()
-{
-    // La funcion principal recibe 3 valores que representan los lados de una figura: determinar, en caso de ser un triangulo, de qué tipo se trata
-
-    int s1, s2, s3, res;
-    s1 = s2 = s3 = 0;
-    cout << "HOLA USUARIO. A continuación deberá ingresar por teclado los tres valores que representan a los lados de una figura. Si se llega a determinar que es un triángulo, veremos si es equilátero, isósceles o escaleno." << endl;
-    cout << "Ingrese el primer valor: ";
-    cin >> s1;
-    cout << "Ingrese el segundo valor: ";
-    cin >> s2;
-    cout << "Ingrese el terecer valor: ";
-    cin >> s3;
-
-    if (is_triangle(s1, s2, s3))
-    {
-        res = triangle_type(s1, s2, s3);
-        switch (res)
-        {
-        case -1:
-            cout << "Los valores ingresados forman un triángulo isósceles." << endl;
-            break;
-        case 0:
-            cout << "Los valores ingresados forman un triángulo equilátero." << endl;
-            break;
-        case 1:
-            cout << "Los valores ingresados forman un triángulo escaleno." << endl;
-            break;
-        default:
-            break;
-        }
-    }
-    else
-    {
-        cout << "Los valores ingresados no forman un triángulo." << endl;
     }
 }
