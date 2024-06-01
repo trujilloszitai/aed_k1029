2be8db5c31801d3311b6ed9d2ad8f384c28b0cf1
trujilloszitai <trujilloszitai@gmail.com>
2024-06-01 00:17:33 -0300

    p2-ejer01

--- practica2/ejer01/main.cpp
+++ practica2/ejer01/main.cpp
@@ -5,15 +5,23 @@ using namespace std;
 int main()
 {
     int N = 0;
-    while (N>0 && N<30)
+    int nums[30] = {0};
+
+    while (N<=0 && N<30)
     {
-        cout << "Ingrese un número N menor que 30 y distinto de 0:";
+        cout << "Ingrese un número N menor que 30 y distinto de 0: ";
         cin >> N;
     }
 
-    return 0;
-}
+    for (int i = 1; i <= N; i++)
+    {
+        nums[i-1] = i * 2;
+    }
 
-void vec_inf (int n) {
-    
+    for (int i = 0; i < N; i++)
+    {
+        cout << nums[i] << endl;
+    }
+
+    return 0;
 }
