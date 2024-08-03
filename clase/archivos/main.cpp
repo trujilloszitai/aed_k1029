5bb261e2c950efc1ae8357d56d69aa1963ab337e
Francisco Trujillo <trujilloszitai@gmail.com>
2024-08-03 15:10:17 -0300

    clase de archivos

--- clase/archivos/main.cpp
+++ clase/archivos/main.cpp
@@ -2,15 +2,41 @@
 #include <fstream>
 using namespace std;
 
-struct Alumno {
-    char nombre[20+1];
-    int id;
+struct Alumno
+{
+    int legajo;
+    char nombre[50 + 1];
 };
 
-int main() {
-
+int main()
+{
+    // Archivos binarios de estructura, siempre se necesita un struct para leer su estructura
     Alumno al;
-    FILE* f=fopen("alumnos.bin","wb");
-    
+    FILE *f = fopen("alumnos.bin", "wb"); // puede ser rb, wb, rb+, wb+
+
+    al.legajo = 111;
+    strcpy(al.nombre, "Francisco");
+
+    fwrite(&al, sizeof(Alumno), 1, f);
+    f = fopen("alumnos.bin", "rb");
+    fclose(f);
     return 0;
+}
+
+void leerArchivo(FILE*& f,...)
+{
+    Alumno al;
+    /* fread(&al, sizeof(Alumno), 1, f);
+    while(!feof(f)) // Mientras la lectura no llegue a la marca de fin de archivo
+    {
+        cout << "Legajo: " << al.legajo << endl;
+        cout << "Nombre: " << al.nombre << endl;
+        fread(&al, sizeof(Alumno), 1, f);
+    } */
+
+    while (fread(&al, sizeof(Alumno), 1, f)) // Mientras la lectura no llegue a la marca de fin de archivo
+    {
+        cout << "Legajo: " << al.legajo << endl;
+        cout << "Nombre: " << al.nombre << endl;
+    }
 }
