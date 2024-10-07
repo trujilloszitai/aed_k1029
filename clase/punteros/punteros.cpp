b05d4975e14523d704c21715b8c7ae4e1f9f04ba
trujilloszitai <trujilloszitai@gmail.com>
2024-10-07 09:49:05 -0300

    remover bibliotecas

--- clase/punteros/punteros.cpp
+++ clase/punteros/punteros.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
 #include <cstring>
-#include <queue>
-#include <stack>
 using namespace std;
 
 struct Alumno
 {
 	int legajo;
-	char nombre[30+1];	
+	char nombre[30+1];
 };
 
 struct nodo
@@ -24,14 +22,14 @@ int main()
 //	int* p=NULL;
 //	int a=10;
 //	p = &a;
-	
+
 	/* Alumno* p = new Alumno();
 	(*p).legajo = 1;
 	cout << p->legajo; */
-	
+
 	nodo* pila = NULL;
 	Alumno al;
-	
+
 	al.legajo = 11;
 	strcpy(al.nombre, "Federico");
 	push(pila,al);
@@ -39,27 +37,27 @@ int main()
 	al.legajo = 22;
 	strcpy(al.nombre, "Fabian");
 	push(pila,al);
-	
+
 	al.legajo = 33;
 	strcpy(al.nombre, "Franco");
 	push(pila,al);
-	
+
 	while(pila!=NULL)
 	{
 		al = pop(pila);
 		cout << al.legajo << " " << al.nombre << endl;
 	}
-	
+
 	return 0;
 }
 
 void push(nodo*& pila, Alumno al)
 {
 	nodo* p = new nodo();
-	
+
 	p->info = al;
 	p->sgte = pila;
-	
+
 	pila = p;
 }
 
@@ -70,6 +68,6 @@ Alumno pop(nodo*& pila)
 	al = p->info;
 	pila = p->sgte;
 	delete p;
-	
+
 	return al;
 }
